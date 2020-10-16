30e1ccd8a5fa128a398931e1c916d0581b956e88
Elena Vonder Meulen <evonder13@gmail.com>
2020-10-16 15:00:29 -0400

    Final Commit

--- PRSCapstoneDB/Models/RequestLine.cs
+++ PRSCapstoneDB/Models/RequestLine.cs
@@ -9,9 +9,9 @@ namespace PRSCapstoneDB.Models
     {
         public int Id { get; set; }
         public int RequestId { get; set; }
-        public virtual Request request { get; set; }
+        public virtual IEnumerable<Request> request { get; set; }
         public int ProductId { get; set; }
-        public virtual Product product { get; set; }
+        public virtual IEnumerable<Product> product { get; set; }
         public int Quantity { get; set; } = 1;
 
         public RequestLine()
